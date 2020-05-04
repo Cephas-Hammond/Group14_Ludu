280480d11deab44d9d5ca8276ecf2da4a221b9f4
Cephas Hammond <cnahammond002@gmail.com>
2020-05-04 21:57:41 +0000

    Updated NewGame with dice implementation and Added new classes for movement

--- src/sample/MultiPLay.java
+++ src/sample/MultiPLay.java
@@ -7,7 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
+//import javafx.scene.text.Font;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
--- src/sample/NewGame.java
+++ src/sample/NewGame.java
@@ -3,20 +3,63 @@ package sample;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import java.util.*;
 
 public class NewGame {
+    
+    private static Label play;
+    private static int dice;
 
     public static void startNewGame(Stage window){
         Label text = new Label("This is a new Game");
-
+        
+        play = new Label();
+        Button btn = new Button("Roll Dice");
+        btn.setOnAction(e->{
+            rollDice();
+            
+            });
+        
         VBox layout = new VBox(10);
-        layout.getChildren().add(text);
+        layout.getChildren().addAll(text,btn,play);
         layout.setAlignment(Pos.CENTER);
 
         Scene scene = new Scene(layout,800,900);
 
         window.setScene(scene);
+    }
+    private static void rollDice(){
+    Random rand = new Random();
+        dice = rand.nextInt(7);
+        
+
+        switch(dice){
+            case 0:
+            case 1:
+                play.setText("1");
+                break;
+            case 2:
+                play.setText("2");
+                break;
+            case 3:
+                play.setText("3");
+                break;
+            case 4:
+                play.setText("4");
+                break;
+            case 5:
+                play.setText("5");
+                break;
+            case 6:
+                play.setText("6");
+                break;
+
+        }
+    }
+    public static int getDice(){
+        return dice;
     }
 }
